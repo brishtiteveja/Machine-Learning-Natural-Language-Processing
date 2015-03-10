74531e668802948da3bf38a0fb53447dbe2b8e38
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2015-03-09 23:59:18 -0400

    post tagging of twitter data.

--- tagger_compare.py
+++ tagger_compare.py
@@ -4,6 +4,7 @@ import maxentpostagger
 import time
 import os, sys
 import nltk
+import string
 
 from nltk.tag import UnigramTagger, BigramTagger, TrigramTagger
 from nltk.tag import ContextTagger,AffixTagger,RegexpTagger, ClassifierBasedTagger,ClassifierBasedPOSTagger
@@ -12,6 +13,16 @@ from nltk.corpus import brown
 from utils import str_to_class
 from nltk.tag.util import untag
 from nltk.metrics.scores import accuracy,precision, recall, log_likelihood, f_measure
+from nltk.sem.evaluate import Undefined
+  
+from nltk.classify.api import ClassifierI, MultiClassifierI
+from nltk.classify.weka import WekaClassifier, config_weka
+from nltk.classify.naivebayes import NaiveBayesClassifier
+from nltk.classify.positivenaivebayes import PositiveNaiveBayesClassifier
+from nltk.classify.decisiontree import DecisionTreeClassifier
+from nltk.classify.util import accuracy, apply_features, log_likelihood
+from nltk.classify.scikitlearn import SklearnClassifier
+from nltk.classify.maxent import (MaxentClassifier, ConditionalExponentialClassifier)
 
 trainf = open("/Users/ananda/Documents/projects/oct27.splits/oct27.traindev", "r")
 testf = open("/Users/ananda/Documents/projects/oct27.splits/oct27.test", "r")
@@ -22,8 +33,10 @@ for l in trainf:
     l = l.rstrip()
     if l != "":
         wt = l.split("\t")
-        w = wt[0]
-        t = wt[1]
+        wt[0]=filter(lambda x: x in string.printable, wt[0])
+        wt[1]=filter(lambda x: x in string.printable, wt[1])
+        w = (wt[0])
+        t = (wt[1])
         sent.append((w,t))
     if l == "":
         sents.append(sent)
@@ -41,8 +54,10 @@ for l in testf:
     l = l.rstrip()
     if l != "":
         wt = l.split("\t")
-        w = wt[0]
-        t = wt[1]
+        wt[0]=filter(lambda x: x in string.printable, wt[0])
+        wt[1]=filter(lambda x: x in string.printable, wt[1])
+        w = (wt[0])
+        t = (wt[1])
         sent.append((w,t))
     if l == "":
         sents2.append(sent)
@@ -53,7 +68,8 @@ for l in testf:
 
 test = sents2
 
-
+print len(test)
+print ""
 #Brown corpus extraction
 # sents = brown.tagged_sents()
 # words = brown.tagged_words()
@@ -82,7 +98,7 @@ print "Unigram Tagger:"
 print "Training"
 t_s = time.time()
 unigram_tagger = UnigramTagger(training)
-  
+   
 print "Evaluation"
 tagged_sents=unigram_tagger.tag_sents(untag(sent) for sent in test)
 (a,p,r,f) = evaluate(tagged_sents,test)
@@ -90,17 +106,17 @@ print "accuracy: %.1f %%" % (a * 100)
 print "precision: %.1f %%" % (p * 100)
 print "recaull: %.1f %%" % (r * 100)
 print "f_measure: %.1f %%" % (f  * 100)
-  
+   
 t_e = time.time()
 t = t_e - t_s 
 print "Took " + str(t)  + " time"
-  
-  
+   
+   
 print "Bigram Tagger:" 
 print "Training"
 t_s = time.time()
 bigram_tagger = BigramTagger(training, backoff=unigram_tagger) # uses unigram tagger in case it can't tag a word
-  
+   
 print "Evaluation"
 tagged_sents=bigram_tagger.tag_sents(untag(sent) for sent in test)
 (a,p,r,f) = evaluate(tagged_sents,test)
@@ -108,18 +124,18 @@ print "accuracy: %.1f %%" % (a * 100)
 print "precision: %.1f %%" % (p * 100)
 print "recaull: %.1f %%" % (r * 100)
 print "f_measure: %.1f %%" % (f  * 100)
-  
+   
 t_e = time.time()
 t = t_e - t_s 
 print "Took " + str(t)  + " time"
-  
-  
+   
+   
 print "Trigram Tagger:"
 print "Training"
 t_s = time.time()
 trigram_tagger = TrigramTagger(training, backoff=unigram_tagger)
-  
-  
+   
+   
 print "Evaluation"
 tagged_sents=trigram_tagger.tag_sents(untag(sent) for sent in test)
 (a,p,r,f) = evaluate(tagged_sents,test)
@@ -136,31 +152,77 @@ print "Training"
 t_s = time.time()
 import string
 regexp_tagger = RegexpTagger(
-            [(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
-              (r'(The|the|A|a|An|an)$', 'AT'),   # articles
-              (r'.*able$', 'JJ'),                # adjectives
-              (r'.*ness$', 'NN'),                # nouns formed from adjectives
-              (r'.*ly$', 'RB'),                  # adverbs
-              (r'.*s$', 'NNS'),                  # plural nouns
-              (r'.*ing$', 'VBG'),                # gerunds
-              (r'.*ed$', 'VBD'),                 # past tense verbs
-              (r'.*', 'NN'),                      # nouns (default)
-              (r'.*ould$', 'MD'),
-              (r'.*ing$', 'VBG'),
-              (r'.*ed$', 'VBD'),
-              (r'.*ness$', 'NN'),
-              (r'.*ment$', 'NN'),
-              (r'.*ful$', 'JJ'),
-              (r'.*ious$', 'JJ'),
-              (r'.*ble$', 'JJ'),
-              (r'.*ic$', 'JJ'),
-              (r'.*ive$', 'JJ'),
-              (r'.*ic$', 'JJ'),
-              (r'.*est$', 'JJ'),
-              (r'^a$', 'PREP'),
-              (r'^i$', 'PN'),
-              (r'^[A-Z][a-z]$', 'PN'),
-              (r'['+string.punctuation+']', 'PUN')
+            [(r'^-?[0-9]+(.[0-9]+)?$', '$'),   #numberal
+              (r'(The|the|A|a|An|an)$', 'D'),   # articles
+              (r'(^The$|^the$|^A$|^a$|^An$|^an$)$', 'D'),   # articles
+              (r'(Am|am|Is|is|Are|are|Was|was|Were|were|will|shall)$', 'V'),   # auxiliary verbs
+              (r'(^Am$|^am$|^Is$|^is$|^Are$|^are$|^Was$|^was$|^Were$|^were$|^will$|^shall$)$', 'V'),   # auxiliary verbs
+              (r'(^I$|^i$|^me$|^We$|^we$|^us$|^You$|^you$|^He$|^he$|^him$|^her$|^his$|^She$|^she$|^They$|^they$|^Them$|^them$)$', 'D'),   # articles
+              (r'(and|that|but|or|as|if|when|than|because|while|where|after|so|though|since|until|whether|before|although|Although|nor|like|once|unless|until|till|now|except)', '&'),
+              (r'(^and$|^that$|^but$|^or$|^as$|^if$|^when$|^than$|^because$|^while$|^where$|^after$|^so$|^though$|^since$|^until$|^whether$|^before$|^although$|^Although$|^nor$|^like$|^once$|^unless$|^until$|^till$|^now$|^except$)', '&'),
+              (r'(:\-\)|:\-\(|:\-\|\||;\-\)|:\-D:\-\/|:\-P)', 'E'),   # common emoticons
+              (r'^(:\-\)$|^:\-\($|^:\-\|\|$|^;\-\)$|^:\-D$|^:\-\/$|^:\-P$)', 'E'),   # common emoticons
+              (r'^@*$|\s+@*$|@', '@$'),   # @mark
+              (r'^#*$|\s+#*$|^#*$|#', '#'),   # @mark
+              (r'.*\'m$','L'),
+              (r'.*\'s$','L'),
+              (r'.*\'re$','L'),  
+              (r'.^There$', 'X'),  
+              (r'.*able$', 'A'),                # adjectives
+              (r'.*ness$', 'N'),                # nouns formed from adjectives
+              (r'.*ly$', 'R'),                  # adverbs
+              (r'.*ily$', 'R'),                  # adverbs
+              (r'.*ly$', 'R'),                  # adverbs
+              (r'.*ically$', 'R'),                  # adverbs
+              (r'.*lly$', 'R'),                  # adverbs
+              (r'.*es$', 'V'),                  # plural nouns
+              (r'.*ing$', 'V'),
+              (r'.*ed$', 'V'),
+              (r'.*ate$', 'V'),
+              (r'.*en$', 'V'),
+              (r'.*ize$', 'V'),
+              (r'.*ise$', 'V'),
+              (r'.*yze$', 'V'),
+              (r'.*ize$', 'V'),
+              (r'.*ed$', 'V'),
+              (r'.*s$', 'N'),                  # plural nouns
+              (r'.*ing$', 'V'),                # gerunds
+              (r'.*ed$', 'V'),                 # past tense verbs
+              (r'.*ould$', 'V'),
+              (r'.*ness$', 'N'),
+              (r'.*ment$', 'N'),
+              (r'.*al$', 'N'),
+              (r'.*ance$', 'N'),
+              (r'.*ence$', 'N'),
+              (r'.*ation$', 'N'),
+              (r'.*sion$', 'N'),
+              (r'.*tion$', 'N'),
+              (r'.*ure$', 'N'),
+              (r'.*ity$', 'N'),
+              (r'.*age$', 'N'),
+              (r'.*ship$', 'N'),
+              (r'.*acy$', 'N'),
+              (r'.*ability$', 'N'),
+              (r'.*ing$', 'N'),
+              (r'.*ery$', 'N'),
+              (r'.*ful$', 'R'),
+              (r'.*ious$', 'R'),
+              (r'.*ble$', 'A'),
+              (r'.*ic$', 'A'),
+              (r'.*cy$', 'A'),
+              (r'.*ive$', 'A'),
+              (r'.*ic$', 'A'),
+              (r'.*ical$', 'A'),
+              (r'.*ish$', 'A'),
+              (r'.*less$', 'A'),
+              (r'.*like$', 'A'),
+              (r'.*y$', 'A'),
+              (r'.*ous$', 'A'),
+              (r'.*est$', 'A'),
+              (r'^a$', 'P'),
+              (r'^i$', 'P'),
+              (r'['+string.punctuation+']', ','),
+              (r'.*','G')
          ])
  
 print "Evaluation"
@@ -189,21 +251,94 @@ print "recaull: %.1f %%" % (r * 100)
 print "f_measure: %.1f %%" % (f  * 100)
 t_e = time.time()
 t = t_e - t_s 
-# print "Took " + str(t)  + " time"
+print "Took " + str(t)  + " time"
   
+
+
+
+#Various backoff tagger
+def backoff_tagger(tagged_sents, tagger_classes, backoff=None):
+    if not backoff:
+        backoff = tagger_classes[0](tagged_sents)
+        del tagger_classes[0]
   
-from nltk.classify.api import ClassifierI, MultiClassifierI
-from nltk.classify.weka import WekaClassifier, config_weka
-from nltk.classify.naivebayes import NaiveBayesClassifier
-from nltk.classify.positivenaivebayes import PositiveNaiveBayesClassifier
-from nltk.classify.decisiontree import DecisionTreeClassifier
-from nltk.classify.util import accuracy, apply_features, log_likelihood
-from nltk.classify.scikitlearn import SklearnClassifier
-from nltk.classify.maxent import (MaxentClassifier, ConditionalExponentialClassifier)
+    for cls in tagger_classes:
+        tagger = cls(tagged_sents, backoff=backoff)
+        backoff = tagger
+  
+    return backoff
+ 
+ 
+seq_taggers = ["ubt_tagger", "utb_tagger", "but_tagger", "btu_tagger", "tub_tagger", "tbu_tagger",
+                "ubta_tagger", "ubat_tagger", "uabt_tagger", "aubt_tagger", "raubt_tagger"]
+ 
+def ubt_tagger():
+    return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.TrigramTagger])
+def utb_tagger(): 
+    return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.TrigramTagger, nltk.tag.BigramTagger])
+def but_tagger():
+    return backoff_tagger(training, [nltk.tag.BigramTagger, nltk.tag.UnigramTagger, nltk.tag.TrigramTagger])
+def btu_tagger():
+    return backoff_tagger(training, [nltk.tag.BigramTagger, nltk.tag.TrigramTagger, nltk.tag.UnigramTagger])
+def tub_tagger():
+    return backoff_tagger(training, [nltk.tag.TrigramTagger, nltk.tag.UnigramTagger, nltk.tag.BigramTagger])
+def tbu_tagger():
+    return backoff_tagger(training, [nltk.tag.TrigramTagger, nltk.tag.BigramTagger, nltk.tag.UnigramTagger])
+def ubta_tagger():
+    return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.TrigramTagger, nltk.tag.AffixTagger])
+def ubat_tagger():
+    return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.AffixTagger, nltk.tag.TrigramTagger])
+def uabt_tagger():
+    return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.AffixTagger, nltk.tag.TrigramTagger])
+def aubt_tagger():
+    return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.AffixTagger, nltk.tag.TrigramTagger])
+def raubt_tagger():
+    return backoff_tagger(training, [nltk.tag.AffixTagger, nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.TrigramTagger],
+    backoff=regexp_tagger)
+ 
+ 
+for i in range(len(seq_taggers)):
+    print "Sequence Tagger(" + seq_taggers[i] +"):"
+    print "Training"
+    t_s = time.time()
+    methodToCall = getattr(sys.modules[__name__],seq_taggers[i])
+    seqt_tagger = methodToCall();
+    print "Evaluation"
+    tagged_sents = seqt_tagger.tag_sents(untag(sent) for sent in test)
+    (a,p,r,f) = evaluate(tagged_sents,test)
+    print "SequenceTagger(" + seq_taggers[i] + ") accuracy: %.1f %%" % (a * 100)
+    print "SequenceTagger(" + seq_taggers[i] + ") precision: %.1f %%" % (p * 100)
+    print "SequenceTagger(" + seq_taggers[i] + ") recaull: %.1f %%" % (r * 100)
+    print "SequenceTagger(" + seq_taggers[i] + ") f_measure: %.1f %%" % (f  * 100)
+        
+    t_e = time.time()
+    t = t_e - t_s 
+    print "Took " + str(t)  + " time"
+    print " "
+ 
+ 
+  
+print "HMM Tagger:"
+print "Training"
+t_s = time.time()
+hmm_tagger = HiddenMarkovModelTagger.train(training)
+  
+print "Evaluation"
+tagged_sents=hmm_tagger.tag_sents(untag(sent) for sent in test)
+(a,p,r,f) = evaluate(tagged_sents,test)
+print "accuracy: %.1f %%" % (a * 100)
+print "precision: %.1f %%" % (p * 100)
+print "recaull: %.1f %%" % (r * 100)
+print "f_measure: %.1f %%" % (f  * 100)
+t_e = time.time()
+t = t_e - t_s 
+print "Took " + str(t)  + " time"
+
+
                                      
 #classifiers = ("NaiveBayesClassifier", "WekaClassifier", "PositiveNaiveBayesClassifier",
 #                "DecisionTreeClassifier","MaxentClassifier","SklearnClassifier", "ConditionalExponentialClassifier" )
-classifiers = ("DecisionTreeClassifier","MaxentClassifier","SklearnClassifier", "ConditionalExponentialClassifier" )
+classifiers = ("NaiveBayesClassifier","DecisionTreeClassifier","MaxentClassifier","SklearnClassifier", "ConditionalExponentialClassifier" )
    
 clsf = [str_to_class(s) for s in classifiers]
   
@@ -225,82 +360,3 @@ for i in range(len(clsf)):
     t = t_e - t_s 
     print "Took " + str(t)  + " time"
     print " "
-
-
-#Various backoff tagger
-# def backoff_tagger(tagged_sents, tagger_classes, backoff=None):
-#     if not backoff:
-#         backoff = tagger_classes[0](tagged_sents)
-#         del tagger_classes[0]
-#  
-#     for cls in tagger_classes:
-#         tagger = cls(tagged_sents, backoff=backoff)
-#         backoff = tagger
-#  
-#     return backoff
-# 
-# 
-# seq_taggers = ["ubt_tagger", "utb_tagger", "but_tagger", "btu_tagger", "tub_tagger", "tbu_tagger",
-#                 "ubta_tagger", "ubat_tagger", "uabt_tagger", "aubt_tagger", "raubt_tagger"]
-# 
-# def ubt_tagger():
-#     return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.TrigramTagger])
-# def utb_tagger(): 
-#     return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.TrigramTagger, nltk.tag.BigramTagger])
-# def but_tagger():
-#     return backoff_tagger(training, [nltk.tag.BigramTagger, nltk.tag.UnigramTagger, nltk.tag.TrigramTagger])
-# def btu_tagger():
-#     return backoff_tagger(training, [nltk.tag.BigramTagger, nltk.tag.TrigramTagger, nltk.tag.UnigramTagger])
-# def tub_tagger():
-#     return backoff_tagger(training, [nltk.tag.TrigramTagger, nltk.tag.UnigramTagger, nltk.tag.BigramTagger])
-# def tbu_tagger():
-#     return backoff_tagger(training, [nltk.tag.TrigramTagger, nltk.tag.BigramTagger, nltk.tag.UnigramTagger])
-# def ubta_tagger():
-#     return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.TrigramTagger, nltk.tag.AffixTagger])
-# def ubat_tagger():
-#     return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.AffixTagger, nltk.tag.TrigramTagger])
-# def uabt_tagger():
-#     return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.AffixTagger, nltk.tag.TrigramTagger])
-# def aubt_tagger():
-#     return backoff_tagger(training, [nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.AffixTagger, nltk.tag.TrigramTagger])
-# def raubt_tagger():
-#     return backoff_tagger(training, [nltk.tag.AffixTagger, nltk.tag.UnigramTagger, nltk.tag.BigramTagger, nltk.tag.TrigramTagger],
-#     backoff=regexp_tagger)
-# 
-# 
-# for i in range(len(seq_taggers)):
-#     print "Sequence Tagger(" + seq_taggers[i] +"):"
-#     print "Training"
-#     t_s = time.time()
-#     methodToCall = getattr(sys.modules[__name__],seq_taggers[i])
-#     seqt_tagger = methodToCall();
-#     print "Evaluation"
-#     tagged_sents = seqt_tagger.tag_sents(untag(sent) for sent in test)
-#     (a,p,r,f) = evaluate(tagged_sents,test)
-#     print "SequenceTagger(" + seq_taggers[i] + ") accuracy: %.1f %%" % (a * 100)
-#     print "SequenceTagger(" + seq_taggers[i] + ") precision: %.1f %%" % (p * 100)
-#     print "SequenceTagger(" + seq_taggers[i] + ") recaull: %.1f %%" % (r * 100)
-#     print "SequenceTagger(" + seq_taggers[i] + ") f_measure: %.1f %%" % (f  * 100)
-#        
-#     t_e = time.time()
-#     t = t_e - t_s 
-#     print "Took " + str(t)  + " time"
-#     print " "
-# 
-# 
-#  
-# print "HMM Tagger:"
-# print "Training"
-# t_s = time.time()
-# hmm_tagger = HiddenMarkovModelTagger.train(training)
-#  
-# print "Evaluation"
-# tagged_sents=hmm_tagger.tag_sents(untag(sent) for sent in test)
-# (a,p,r,f) = evaluate(tagged_sents,test)
-# print "accuracy: %.1f %%" % (a * 100)
-# print "precision: %.1f %%" % (p * 100)
-# print "recaull: %.1f %%" % (r * 100)
-# print "f_measure: %.1f %%" % (f  * 100)
-# t_e = time.time()
-# t = t_e - t_s 
-# print "Took " + str(t)  + " time"
